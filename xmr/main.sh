012568de2bf820602ac3e77e6c905740d26a8262
xorchi <jperkasa8@gmail.com>
2026-03-28 10:35:31 +0700

    update xmr/main.sh

--- xmr/main.sh
+++ xmr/main.sh
@@ -173,8 +173,12 @@ if [[ ! -f "$WORKDIR/$WALLET_NAME" || ! -f "$WORKDIR/$WALLET_NAME.keys" ]]; then
     decrypt
 fi
 
-#read -rp "> " choice
-choice=1
+echo "Select mode to run monero-wallet-cli:"
+echo "1) Tor (onion node)"
+echo "2) Clearnet (clearnet node)"
+echo "3) Offline (no network)"
+read -rp "Your choice: " choice
+#choice=1
 case "$choice" in
     1) run_monero tor      ;;
     2) run_monero clearnet ;;
